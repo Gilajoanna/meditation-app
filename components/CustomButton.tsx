00ee80708bfe4ffa1fc284cbc327e660cf8bd615
Gila Johansson <gilajohansson@Gila-sin-MacBook-Pro.local>
2024-11-27 14:52:27 +0100

    CustomButton component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+
+interface CustomButtonProps {
+    onPress: () => void;
+    title: string;
+    textStyles?: string;
+    containerStyles?: string;
+}
+
+const CustomButton = ({ onPress, title, textStyles = "", containerStyles = "" }: CustomButtonProps) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7}
+    style={styles.button}
+    className={containerStyles}
+    onPress={onPress}
+    >
+      <Text style={styles.buttonText} className={textStyles}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor: "white",
+      minHeight: 62,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      opacity: 0.7,
+    },
+    buttonText: {
+      color: "black",
+      fontWeight: "bold",
+      fontSize: 16,
+    },
+});
