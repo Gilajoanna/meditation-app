609d180d46b5331b465d170a3bacff27731e28a0
Gila Johansson <gilajohansson@Gila-sin-MacBook-Pro.local>
2024-11-27 21:46:58 +0100

    nature-meditate showing list with different meditate list items

--- app/nature-meditate.tsx
+++ app/nature-meditate.tsx
@@ -1,10 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Pressable, ImageBackground } from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
+import { StatusBar } from 'expo-status-bar'
+import { MEDITATION_DATA } from "@/constants/MeditationData";
+import MEDITATION_IMAGES from "@/constants/meditation-images";
 
 const NatureMeditate = () => {
   return (
     <View style={styles.container}>
-      <Text>Nature Meditate</Text>
+        <AppGradient colors={["#161b2e", "#344C64", "#577B8D", "#57A6A1"]}>
+            <View style={styles.textContainer}>
+                <Text style={styles.titleText}>Welcome, Gila</Text>
+                <Text style={styles.subText}>Begin your meditation journey today.</Text>
+            </View>
+
+            <View>
+                <FlatList style={styles.list}
+                showsVerticalScrollIndicator={false}
+                data={MEDITATION_DATA} 
+                keyExtractor={(item) => item.id.toString()} 
+                renderItem={({ item }) => (
+                    <Pressable style={styles.listItem}
+                    onPress={() => console.log("Pressed item")}>
+                        <ImageBackground source={MEDITATION_IMAGES[item.id -1]} style={styles.listItemImage}>
+                            <Text style={styles.listItemText}>{item.title}</Text>
+                        </ImageBackground>
+                    </Pressable>
+                )}
+                />
+            </View>
+        </AppGradient>
+
+        <StatusBar style="light" />
     </View>
   )
 }
@@ -14,7 +41,50 @@ export default NatureMeditate
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
+    },
+    textContainer: {
+        marginTop: 15,
+        marginBottom: 5,
+    },
+    titleText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 28,
+        textAlign: "left",
+        marginBottom: 5,
+        opacity: 0.8,
+    },
+    subText: {
+        color: "white",
+        fontWeight: "medium",
+        fontSize: 16,
+        textAlign: "left",
+        opacity: 0.8,
+    },
+    list: {
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    listItem: {
+        flex: 1,
+        height: 200,
+        borderRadius: 10,
+        overflow: "hidden",
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: "white",
+    },
+    listItemImage: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+    },
+    listItemText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 24,
+        textAlign: "center",
+        //backgroundColor: "rgba(0, 0, 0, 0.2)",
     },
 });
