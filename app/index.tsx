30bed4329a97912e6cd77720c22f9e54a791466f
Gila Johansson <gilajohansson@Gila-sin-MacBook-Pro.local>
2024-11-27 12:48:05 +0100

    Background image, linear gradient package

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,55 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
-
-import forestImage from "@/assets/meditation-images/ancient-enchanted-forest.png";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={forestImage} resizeMode="cover">
-
-        <Text>App</Text>
+      {/* Background Image */}
+      <ImageBackground
+        source={require("@/assets/meditation-images/ancient-enchanted-forest.png")}
+        style={styles.backgroundImage}
+      >
+        <LinearGradient
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+          style={styles.gradient}
+        >
+          {/* Overlay content */}
+          <SafeAreaView style={styles.safeArea}>
+            <View>
+              <Text style={styles.text}>App</Text>
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
     </View>
-  )
-}
-
-export default App
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    imageBackground: {
-      flex: 1,
-      resizeMode: "cover",
+  container: {
+    flex: 1,
   },
-    });
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  gradient: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 28,
+    textAlign: "center",
+  },
+});
+
+export default App;
